5209c1f5e48756d924387eb3183899fc34ff8c56
watchegsmart <smyr91380@gmail.com>
2026-07-05 19:18:49 +0300

    Delete src directory

--- src/socket.js
+++
@@ -1,9 +0,0 @@
-// src/socket.js
-import { io } from "socket.io-client";
-
-const SERVER_URL = process.env.REACT_APP_SERVER_URL || "https://dallah-server.onrender.com";
-
-export const socket = io(SERVER_URL, {
-  transports: ["websocket", "polling"],
-  autoConnect: false,
-});
