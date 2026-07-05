93d5a728f1af32a4e17ef161bea8edf74e2ce994
watchegsmart <smyr91380@gmail.com>
2026-07-05 19:26:19 +0300

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const SERVER_URL = process.env.REACT_APP_SERVER_URL || "https://server-2h6g.onrender.com";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || "https://dallah-server-xpls.onrender.com";
 
 export const socket = io(SERVER_URL, {
   transports: ["websocket", "polling"],
